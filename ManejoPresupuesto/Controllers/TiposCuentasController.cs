c135330ba4df499edd03b95524e51c4b06a6eb22
Jesús Iván Domínguez Torres <jdominguez@seslp.gob.mx>
2022-10-11 15:00:45 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,23 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
